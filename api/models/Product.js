d202d7caccd35ca85808233f79413fb8937fc110
mac <mac@MUKHTERS-MACBOOK.local>
2023-08-08 23:39:24 +0600

    some change

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,59 +1,69 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-  productName: {
-    type: String,
-    required: true,
-    lowercase: true,
-  },
-  categoryId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-    required: true,
-  },
-  subCategoryId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "SubCategory",
-    required: true,
-  },
-  vendorEmail: {
-    type: String,
-    required: true,
-    lowercase: true,
-  },
-  availableQuantity: {
-    type: Number,
-    required: true,
-  },
-  buyingPrice: {
-    type: Number,
-    required: true,
-  },
-  sellingPrice: {
-    type: Number,
-    required: true,
-  },
-  details: {
-    type: String,
-    required: true,
-    lowercase: true,
-  },
-  model: {
-    type: String,
-  },
-  color: {
-    type: String,
-  },
-  size: {
-    type: String,
-  },
-  productImg: [
-    {
+const productSchema = mongoose.Schema(
+  {
+    productName: {
       type: String,
       required: true,
+      lowercase: true,
     },
-  ],
-});
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    subCategoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SubCategory",
+      required: true,
+    },
+    vendorEmail: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    availableQuantity: {
+      type: Number,
+      required: true,
+    },
+    buyingPrice: {
+      type: Number,
+      required: true,
+    },
+    sellingPrice: {
+      type: Number,
+      required: true,
+    },
+    details: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    model: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    size: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "active", "inactive"],
+      default: "pending",
+    },
+    productImg: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
